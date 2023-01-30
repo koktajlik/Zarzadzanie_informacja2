a13f1037465f594f689c8d954cd224c2c18dc5d3
koktajlik <wera.oliwia3@gmail.com>
2023-01-30 01:05:05 +0100

    Added classes

---
+++ src/lab07 c#/ClassLibrary1/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary1;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public virtual DbSet<Customers> Customers { get; set; } = null!;
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL($"server=localhost;database=zi_lab7;uid=root;password=;");
+    }
+}
---
+++ src/lab07 c#/ClassLibrary1/DbModels/Customers.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary1.DbModels;
+
+public class Customers
+{
+    [Key] 
+    public int customerNumber { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string customerName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string contactLastName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string contactFirstName { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string phone { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string addressLine1 { get; set; }
+    [StringLength(50)]
+    public string? addressLine2 { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string city { get; set; }
+    [StringLength(50)]
+    public string? state { get; set; }
+    [StringLength(50)]
+    public string? postalCode { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string country { get; set; }
+    public int? salesRepEmployeeNumber { get; set; }
+    public decimal creditLimit { get; set; }
+}
